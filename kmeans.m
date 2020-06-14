0b67882934188c580bf6f1012087f4eec0b5e8a2
hrishitchaudhuri <57035481+hrishitchaudhuri@users.noreply.github.com>
2020-06-14 10:58:12 +0530

    Update kmeans.m

--- kmeans.m
+++ kmeans.m
@@ -23,6 +23,8 @@ k = 3;
 figure(1);
 
 for i = 1:1:NUM_NODES
+
+    % Distribute nodes randomly over field.
     S(i, 1) = rand(1, 1) * x_max;
     S(i, 2) = rand(1, 1) * y_max;
     
@@ -35,8 +37,11 @@ disp(S);
 plot(sink.x, sink.y, 'black x');
 hold on;
 
+% Apply k-means clustering to divide the network into clusters.
 [idx, C] = kmeans(S, k);
 disp(idx);
 disp(C);
+
+% Plot out the centroids of each cluster.
 plot(C(:,1), C(:,2), 'black x', 'MarkerSize', 15);
 hold on;
