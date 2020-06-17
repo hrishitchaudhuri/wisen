7d800ba241abd33418282574b7e9b42fbeabae76
AmoghKaipa <66296146+AmoghKaipa@users.noreply.github.com>
2020-06-17 12:39:02 +0530

    Update static_sink_model.m

--- static_sink_model.m
+++ static_sink_model.m
@@ -1,103 +1,242 @@
-clc;clear;close;
-%%%%%%%%%%%%%%%%%%%%%%%%% PARAMETERS %%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%Field Dimensions - x and y maximum (in meters)
-xm=100;
-% %% 
-%% 
-ym=100;
-%x and y Coordinates of the Sink  
-sink.x=0.5*xm;
-sink.y=0.5*ym;
-%Number of Nodes in the field  
-NUM_NODES=50;
-NUM_ROUNDS = 25
-%Energy Model (all values in Joules)
-%Initial Energy 
+clc; clear all; close all;
+
+%%%%%%%%%%%%%%%%%%%% PARAMETERS %%%%%%%%%%%%%%%%%%%%
+
+% Field Dimensions
+x_max = 100;
+y_max = 100;
+
+% Sink Coordinates
+sink.x = 0.5 * x_max;
+sink.y = 0.5 * y_max;
+
+% Number of Nodes
+NUM_NODES =100;
+
+% Number of Clusters
+k = 1;
+
+% Initial Energy of Each Node
 Eo=0.5;
-%Eelec=Etx=Erx 
+
+% Packet Length
+packet_length = 500;
+
+% Radio Transmission and Reception Coefficients
 ETX=50*0.0000001;
 ERX=50*0.0000001;
-%Transmit Amplifier types 
-Efs=10*0.0000000001;
-Emp=0.0013*0.0000000001;
-%Data Aggregation Energy/ 
-EDA=5*0.000000001;
-%\alpha
-%% 
-a=1;
-%%%%%%%%%%%%%%%%%%%%%%%%% END OF PARAMETERS %%%%%%%%%%%%%%%%%%%%%%%%
-%Computation of do
-do=sqrt(Efs/Emp);
-%%
-% 
-%   for x = 1:10
-%       disp(x)
-%   end
-% 
-%% 
-%% 
-X=zeros;
-Y=zeros;
 
-[nodes,pos_nodes] = initialise(NUM_NODES);
-plot(sink.x, sink.y, "red ^");
-hold off;
-%Energy dissipated by the nodes
-[energy_nodes, energy_nodes_rounds] = energy_parameters(NUM_NODES, nodes, sink, NUM_ROUNDS);
+% Free Space and Multi-Path Model Coefficients
+Efs= 10*0.0000000001;
+Emp= 0.013*0.0000000001;
 
-node_num = input("Enter node number for viewing its energy statics");
-figure (2);
-i=1:NUM_NODES;
-plot(i, energy_nodes_rounds(node_num));
-%xlim([1 50]);
-%ylim([0.45 0.55]);
-xlabel('Rounds');
-ylabel('Energy');
-title('Static sink');
+% Number of Rounds
+rounds = 100;
 
+% Threshold Distance
+threshold = 1600;
 
+%%%%%%%%%%%%%%%%%%%% END OF PARAMETERS %%%%%%%%%%%%%%%%%%%%
 
-%%%%%%%%%%%% FUNCTIONS %%%%%%%%%%%%%%
-function[nodes,pos_nodes] = initialise(no_of_nodes)
-    figure(1);
+% Threshold Distance
+do = Efs/Emp;
+
+% Creating a random sensor network
+figure(1);
+
+for i = 1:1:NUM_NODES
+    S(i, 1) = rand(1, 1) * x_max;
+    S(i, 2) = rand(1, 1) * y_max;
+    
+    plot(S(i, 1), S(i, 2), 'red o');
     hold on;
-     for i=1:1:no_of_nodes
-        nodes(i) = Sensor_Node;
-        nodes(i)= Add_Positions(nodes(i));
-        pos_nodes(i,1)=nodes(i).xd;
-        pos_nodes(i,2)=nodes(i).yd;
-        plot(nodes(i).xd,nodes(i).yd,'green o');
-     end
 end
 
-function [energy_nodes, energy_nodes_rounds] = energy_parameters(no_of_nodes, NODES, sink, NUM_ROUNDS)
-    Eo=0.5;
-    %Eelec=Etx=Erx 
-    ETX=50*0.000000001;
-    ERX=50*0.00000001;
-    %Transmit Amplifier types 
-    Efs=10*0.0000000001;
-    Emp=0.0013*0.0000000001;
-    %Data Aggregation Energy/ 
-    EDA=5*0.000000001;
-    do=sqrt(Efs/Emp);
-    for r=1:NUM_ROUNDS
-        for i=1:no_of_nodes
-            distance=sqrt((NODES(i).xd-sink.x)^2 + (NODES(i).yd-sink.y)^2 );
-            if (distance>do)
-                        NODES(i).E=NODES(i).E- ( ETX*(4096) + Emp*4096*(distance^4)); 
+% disp(S);
+
+% plot(sink.x, sink.y, 'black o');
+% hold on;
+
+[idx, C] = kmeans(S, k);
+
+% disp(idx);
+% disp(C);
+
+plot(C(:,1), C(:,2), 'k*', 'MarkerSize', 15);
+hold on;
+
+% nodes = zeros;
+for i = 1:1:NUM_NODES
+    nodes(i).x = S(i, 1);
+    nodes(i).y = S(i, 2);
+    nodes(i).battery = Eo;
+    nodes(i).cluster = (idx(i) - 1);
+    nodes(i).sink_x = C(idx(i), 1);
+    nodes(i).sink_y = C(idx(i), 2);
+    nodes(i).state = 1;
+    nodes(i).distance = (nodes(i).x - nodes(i).sink_x)^2 + (nodes(i).y - nodes(i).sink_y)^2;
+    nodes(i).route = zeros;
+end
+
+G = graph();
+for i = 1:1:NUM_NODES
+    if(findnode(G,i)==0)
+    G=addnode(G,i);
+    end
+    for j = i:1:NUM_NODES
+        if (nodes(i).cluster == nodes(j).cluster&&(i~=j))
+            if((nodes(i).distance<=threshold)&&(findnode(G,NUM_NODES+nodes(i).cluster+1)~=0)&&(findedge(G,i,NUM_NODES + nodes(i).cluster + 1)==0))
+                G = addedge(G, i, NUM_NODES + nodes(i).cluster + 1, nodes(i).distance);
+            elseif(findnode(G,NUM_NODES+nodes(i).cluster+1)==0)
+                G = addedge(G, i, NUM_NODES + nodes(i).cluster + 1, nodes(i).distance);
             end
-            %% 
-            % DESCRIPTIVE TEXT
-            if (distance<=do)
-                        NODES(i).E=NODES(i).E- ( ETX*(4096) + Efs*4096*(distance^2)); 
+
+            node_dist = ((nodes(i).x - nodes(j).x)^2 + (nodes(i).y - nodes(j).y)^2);
+            if node_dist < threshold
+                G = addedge(G, i, j, node_dist);
             end
-            energy_nodes(i)= NODES(i).E;
-            energy_nodes_rounds(i)= i, NODES(i).E;
-            disp(NODES(i).E);
         end
     end
 end
 
+for i = 1:1:NUM_NODES
+    nodes(i).route = shortestpath(G, i, NUM_NODES + nodes(i).cluster + 1);
+end
 
+% distances = zeros;
+% for i = 0:1:k-1
+%     for j = 1:1:NUM_NODES
+%         if nodes(j).cluster == i && nodes(i).distance < do
+%         end
+%     end
+% end
+
+wake_clust = 1;
+plot_idx = 1;   
+round_stat = zeros;
+dead_stats = zeros;
+num_temp = NUM_NODES;
+dead_nodes = 0;
+nodes_without_link=0;
+non_link_nodes = 0;
+
+for j=0:1:k-1
+    v = dfsearch(G,NUM_NODES+j+1);
+    for i=1:1:NUM_NODES       
+        if(nodes(i).cluster==(j))
+        check_for_dead_node = ismember(i,v);
+        if(check_for_dead_node==0)
+        nodes_without_link = nodes_without_link+1;
+        non_link_nodes(numel(non_link_nodes)+1) = i;
+        end
+        end
+    end
+end
+
+non_link_nodes(1) = [];
+
+
+while dead_nodes < NUM_NODES
+    dead_nodes = 0;
+    for j = 1:1:num_temp
+        if (nodes(j).state == 1) & (nodes(j).cluster == wake_clust)
+            for m = 1:1:length(nodes(j).route)-1
+                if (nodes(j).route(m+1) <= NUM_NODES)  
+                    if (nodes(nodes(j).route(m)).state == 1 && nodes(nodes(j).route(m+1)).state == 1)
+                    
+                    dist_node = ((nodes(nodes(j).route(m)).x - nodes(nodes(j).route(m+1)).x)^2 + (nodes(nodes(j).route(m)).y - nodes(nodes(j).route(m+1)).y)^2);
+                    if (dist_node > do)
+                        nodes(nodes(j).route(m)).battery = nodes(nodes(j).route(m)).battery - ((ETX)*(packet_length) + Emp*packet_length*(dist_node^2)); 
+                    end
+        
+                    if (dist_node <= do)
+                        nodes(nodes(j).route(m)).battery = nodes(nodes(j).route(m)).battery - ((ETX)*(packet_length) + Efs*packet_length*(dist_node)); 
+                    end
+        
+                    if (nodes(j).route(m+1) <= NUM_NODES)   
+                        nodes(nodes(j).route(m+1)).battery = nodes(nodes(j).route(m+1)).battery - (ERX)*(packet_length);
+                    end
+                    
+                    end
+                
+                
+                    if nodes(nodes(j).route(m)).battery <= 0.1*Eo
+                        nodes(nodes(j).route(m)).state = 0;
+                        plot(nodes(nodes(j).route(m)).x, nodes(nodes(j).route(m)).y, 'black x');
+                        hold on;
+                    end
+                
+                    if nodes(nodes(j).route(m+1)).battery <= 0.1*Eo
+                        nodes(nodes(j).route(m+1)).state = 0;
+                        plot(nodes(nodes(j).route(m+1)).x, nodes(nodes(j).route(m+1)).y, 'black x');
+                        hold on;
+                    end
+                
+                else
+                    if (nodes(nodes(j).route(m)).distance > do)
+                        nodes(nodes(j).route(m)).battery = nodes(nodes(j).route(m)).battery - ((ETX)*(packet_length) + Emp*packet_length*(nodes(nodes(j).route(m)).distance^2)); 
+                    end
+        
+                    if (nodes(nodes(j).route(m)).distance <= do)
+                        nodes(nodes(j).route(m)).battery = nodes(nodes(j).route(m)).battery - ((ETX)*(packet_length) + Efs*packet_length*(nodes(nodes(j).route(m)).distance)); 
+                    end
+                    
+                    if nodes(nodes(j).route(m)).battery <= 0.1*Eo
+                        nodes(nodes(j).route(m)).state = 0;
+                        plot(nodes(nodes(j).route(m)).x, nodes(nodes(j).route(m)).y, 'black x');
+                        hold on;
+                    end
+                end
+                
+            end
+        end
+    end
     
+    for p_0 = 1:1:NUM_NODES
+        if (nodes(p_0).state == 0) & (findedge(G, p_0, nodes(p_0).cluster + NUM_NODES + 1) ~= 0)
+            G = rmedge(G, p_0, nodes(p_0).cluster + NUM_NODES + 1);
+        end
+        
+        for p_1 = p_0:1:NUM_NODES
+            if (findedge(G, p_0, p_1) ~= 0) & (nodes(p_1).state == 0) || nodes(p_0).state == 0
+                G = rmedge(G, p_0, p_1);
+            end
+        end
+    end
+    
+    for p = 1:1:NUM_NODES
+        nodes(p).route = shortestpath(G, p, nodes(p).cluster + NUM_NODES + 1);
+        if nodes(p).state == 0
+            dead_nodes = dead_nodes + 1;
+        end
+        if((isempty(nodes(p).route)==1)&&nodes(p).state==1)
+            nodes_without_link=nodes_without_link+1;
+            nodes(p).state=0;
+        end
+    end
+    
+%     pause(0.5);
+    wake_clust = mod(wake_clust + 1, k);
+    
+    if (mod(i, k) == 0)
+        round_stat(plot_idx) = plot_idx;
+        dead_stats(plot_idx) = dead_nodes;
+        plot_idx = plot_idx + 1;
+    end
+    
+    % if (dead_nodes == NUM_NODES)
+    %     break;
+    % end
+    dead_nodes = nodes_without_link+dead_nodes;
+    disp(dead_nodes);
+    nodes_without_link = 0;
+end
+
+
+
+figure(2);
+plot(G);
+
+figure(3);
+plot(round_stat, dead_stats);
+hold on;
