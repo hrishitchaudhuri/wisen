661e6d9aeb6f0f90b076e1dad64990c4699918a7
AmoghKaipa <66296146+AmoghKaipa@users.noreply.github.com>
2020-07-28 22:44:37 +0530

    Update updated_kmeans.m

--- non-modularized-code/updated_kmeans.m
+++ non-modularized-code/updated_kmeans.m
@@ -14,29 +14,30 @@ sink.y = 0.5 * y_max;
 NUM_NODES =100;
 
 % Number of Clusters
-k = 5;
+k = 7;
 
 % Initial Energy of Each Node
 Eo=0.5;
 %Radius of mobile sink path
-radius = 40;
+radius = 25;
 % Packet Length
 packet_length = 500;
 
+
 % Radio Transmission and Reception Coefficients
-ETX=50*0.0000001;
-ERX=50*0.0000001;
+ETX=50*10^(-9);
+ERX=50*10^(-9);
 
 % Free Space and Multi-Path Model Coefficients
 Eelec=50*10^(-9); % units in Joules/bit
 Efs= 10*0.0000000001;
 Emp= 0.013*0.0000000001;
 EDA=5*10^(-9);
-% Number of Rounds
-rounds = 100;
 
-% Threshold Distance
-threshold = 900;
+
+% Transmit Amplifier Types %
+Eamp=100*10^(-12); % units in Joules/bit/m^2 (amount of energy spent by the amplifier to transmit the bits)
+
 
 %%%%%%%%%%%%%%%%%%%% END OF PARAMETERS %%%%%%%%%%%%%%%%%%%%
 
@@ -56,7 +57,9 @@ for i = 1:1:NUM_NODES
     ylabel '(m)';
     hold on;
 end
-
+% cluster = zeros(k, 20000);
+% weight = zeros(k, 20000);
+% CH_s_in_each_round = zeros(k, 20000);
 %%clustering by k-means
 [idx, C] = kmeans(S, k);
 hold on;
@@ -68,6 +71,7 @@ for i = 1:NUM_NODES
     nodes(i).cluster = (idx(i));
     nodes(i).role=0;
     nodes(i).cond=1;
+    nodes(i).rleft=0;
     nodes(i).dst_from_CH=0;
     nodes(i).dist_from_origin=sqrt((nodes(i).x-50)^2+(nodes(i).y-50)^2);
     if (nodes(i).dist_from_origin >= radius)  %%%%Distance of nodes from circumference of the circular path that the mobile sink takes
@@ -78,7 +82,7 @@ for i = 1:NUM_NODES
 end
 
 %%Obtaining array containg node ID's according to cluster numbers
-for i = 1:5
+for i = 1:k
     c=1;
     for j = 1:NUM_NODES
         if (nodes(j).cluster == i)
@@ -93,11 +97,16 @@ operating_nodes = NUM_NODES;
 dead_nodes=0;
 round = 0;
 while dead_nodes < NUM_NODES
+    
     %calculating weight
     for i = 1: k
         counter = 1;
-        for j = 1: NUM_NODES
-            nodes(j).weight = (nodes(j).battery)^2/nodes(j).dcir;
+        for j = 1 : NUM_NODES
+            if(nodes(j).rleft == 0)
+                nodes(j).weight = (nodes(j).battery)^2/nodes(j).dcir;
+            
+            end
+            
             if (nodes(j).cluster == i)
                 
                 weight(counter,i)=nodes(j).weight;
@@ -105,7 +114,15 @@ while dead_nodes < NUM_NODES
             end
         end
     end
-
+    
+        for j = 1:NUM_NODES
+            
+                if(nodes(j).rleft > 0)
+                    nodes(j).rleft = nodes(j).rleft - 1;
+                end
+            
+        end
+    
 
 
 
@@ -113,11 +130,13 @@ while dead_nodes < NUM_NODES
     %%CH election
     for i = 1:k
         [energy_max,ID]=max(weight(:,i));
-        CH_s(i).id=cluster(ID,i);%%Contains ID's of CH's
-        if (nodes(i).cond == 1)
-            nodes(cluster(ID,i)).role=1;
-            CH_s_in_each_round(i,round+1) = CH_s(i).id;
-        end
+        CH_s(i).id=cluster(ID,i);%%Contains ID's of CH's                
+        nodes(cluster(ID,i)).role=1;
+        nodes(cluster(ID,i)).rleft = 10;
+        nodes(cluster(ID,i)).weight = 0;
+        CH_s_in_each_round(i,round+1) = CH_s(i).id;
+            
+        
         
         for j = 1:NUM_NODES
             if (nodes(j).cluster == i)
@@ -129,7 +148,9 @@ while dead_nodes < NUM_NODES
     for i = 1:NUM_NODES
         if (nodes(i).cond == 1)
             if (nodes(i).role == 0)
-                
+%                 nodes(i).battery = nodes(i).battery - (Eelec*packet_length + Eamp*packet_length*nodes(i).dst_from_CH^2);
+              
+                    
                 if (nodes(i).dst_from_CH < do)
                     nodes(i).battery = nodes(i).battery - (packet_length*Eelec + Efs*packet_length*(nodes(i).dst_from_CH)^2);
                 end
@@ -138,12 +159,15 @@ while dead_nodes < NUM_NODES
                 end
             end
             if (nodes(i).role == 1)
+%                nodes(i).battery = nodes(i).battery - (Eelec+EDA)*packet_length;
                 nodes(i).battery = nodes(i).battery - packet_length*CH_s(nodes(i).cluster).num_of_nodes*Eelec;
             end
             
-            if (nodes(i).battery <= 0.1*Eo)
+            if (nodes(i).battery <= 0)
                 nodes(i).cond = 0;
                 dead_nodes = dead_nodes + 1;
+                disp("Dead node ID");
+                disp(i);
                 operating_nodes = operating_nodes - 1;
             end
         
@@ -152,18 +176,27 @@ while dead_nodes < NUM_NODES
     
     end
     %disp(dead_nodes);
-    
+     
     round = round + 1;
     op(round)=operating_nodes;
     disp(round);
     disp(dead_nodes);
+%     if (dead_nodes == 1 )
+%         break;
+%     end
     
+    for i = 1:NUM_NODES
+        if(nodes(i).role == 1)
+            nodes(i).role = 0;
+        end
+    end
+     
 end
 
 %Plotting Simulation Results "Operating Nodes per Round" %
     figure(2)
     plot(1:round,op(1:round),'-r','Linewidth',2);
-    axis([0  20000    0  NUM_NODES])
+    axis([0  20000    0  NUM_NODES]);
     title ({'Updated k_means'; 'Operating Nodes per Round';})
     xlabel 'Rounds ';
     ylabel 'Operational Nodes ';
